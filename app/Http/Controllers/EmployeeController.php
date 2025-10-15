87aa8a66d56ccd7aa09c22cf05cd84d3e5b30b0c
Jonathan <jonathanlukas17116@gmail.com>
2025-10-15 20:52:52 +0700

    beresin tampilan crud employee, beresin departments

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -21,6 +21,19 @@ class EmployeeController extends Controller
         return view('employees.create', compact('departments', 'roles'));
     }
 
+    public function show($id){
+        $employee = Employee::findOrFail($id);
+        return view('employees.show', compact('employee'));
+    }
+
+    public function edit($id){
+        $employee = Employee::findOrFail($id);
+        $departments = Department::all();
+        $roles = Role::all();
+
+        return view('employees.edit', compact('employee', 'departments', 'roles'));
+    }
+
     public function store(Request $request){
         $request->validate([
             'fullname' => 'required|string|max:255',
@@ -38,5 +51,26 @@ class EmployeeController extends Controller
         Employee::create($request->all());
 
         return redirect()->route('employees.index')->with('success', 'Employee created successfully');
+
     }
+
+     public function update(Request $request, $id){
+            $request->validate([
+                'fullname' => 'required|string|max:255',
+                'email' => 'required|email',
+                'phone_number' => 'required|string|max:20',
+                'address' => 'nullable|required',
+                'birth_date' => 'required|date',
+                'hire_date' => 'required|date',
+                'department_id' => 'required',
+                'role_id' => 'required',
+                'status' => 'required|string',
+                'salary' => 'required|numeric'
+            ]);
+
+            $employee = Employee::findOrFail($id);
+            $employee->update($request->all());
+
+            return redirect()->route('employees.index')->with('success', 'Employee updated successfully');
+        }
 }
