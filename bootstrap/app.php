82bb5e2f92235993b16479af7bd8127809be7847
JonathanLukasW <jonathanlukas17116@gmail.com>
2025-11-13 00:03:04 +0700

    benerin spatie

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,23 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        
+        // Ini adalah array "Alias" (shortcut)
         $middleware->alias([
-            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class
-            // 'role' => \App\Http\Middleware\CheckRole::class
-        ]);  
+            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+            
+            // 'guest' and 'auth' aliases are automatically registered 
+            // by Laravel 11/12, so we can remove them from here.
+            
+            // --- PERBAIKANNYA DI SINI ---
+            // Tambahkan 3 baris ini untuk mendaftarkan alias Spatie
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
+
+        // (Mungkin ada konfigurasi middleware lain di sini...)
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,15 +8,15 @@ use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Seed the application's database.
+     */
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => Hash::make('password'), // Tambahkan password yang di-hash
-        ]);
+
         $this->call([
             HumanResourceSeeder::class, 
+            PermissionSeeder::class, // <-- INI YANG KETINGGALAN
         ]);
     }
 }
